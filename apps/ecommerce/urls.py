cb134baa56315be4eb792f2abdb28e949e53c096
eriveltosilva <eriveltoclenio@gmail.com>
2024-03-19 02:28:53 +0100

    feat: preview navigation link Changes to be committed: 	new file:   apps/ecommerce/urls.py 	modified:   apps/ecommerce/views.py 	modified:   setup/urls.py 	new file:   templates/base.html 	deleted:    templates/cart.html 	deleted:    templates/checkout.html 	deleted:    templates/contact.html 	new file:   templates/ecommerce/cart.html 	new file:   templates/ecommerce/checkout.html 	new file:   templates/ecommerce/contact.html 	renamed:    templates/detail.html -> templates/ecommerce/detail.html 	new file:   templates/ecommerce/home.html 	renamed:    templates/shop.html -> templates/ecommerce/shop.html 	deleted:    templates/index.html 	new file:   templates/partials/_footer.html 	new file:   templates/partials/_navbar.html 	new file:   templates/partials/_topbar.html

---
+++ apps/ecommerce/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+app_name="ecommerce"
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('checkout-produtos/', views.checkout, name="checkout"),
+    path('perguntas-frequentes/', views.faqs, name="faqs"),
+    path('sobre-nos/', views.about_us, name="about-us"),
+
+    path('meu-carrinho/', views.cart, name="cart"),
+    path('contactos/', views.contact, name="contact"),
+    path('produtos/detalhes', views.detail, name="detail"),
+    path('produtos/comprar', views.shop, name="shop"),
+    path('produtos/minha-lista-de-desejos', views.wishlist, name="wishlist"),
+]
--- apps/ecommerce/views.py
+++ apps/ecommerce/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+def cart(request):
+    return render(request, 'ecommerce/cart.html')
+def checkout(request):
+    return render(request, 'ecommerce/checkout.html')
+def contact(request):
+    return render(request, 'ecommerce/contact.html')
+def detail(request):
+    return render(request, 'ecommerce/detail.html')
+def home(request):
+    return render(request, 'ecommerce/home.html')
+def shop(request):
+    return render(request, 'ecommerce/shop.html')
+
+def wishlist(request):
+    return render(request, 'ecommerce/wishlist.html')
+def faqs(request):
+    return render(request, 'ecommerce/faqs.html')
+def about_us(request):
+    return render(request, 'ecommerce/about_us.html')
