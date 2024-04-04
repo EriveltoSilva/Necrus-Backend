3d65b63ca1b5d10fd9d3ea0e76cf04e695ecb088
eriveltosilva <eriveltoclenio@gmail.com>
2024-04-04 15:20:30 +0100

    feat: checkout template rendering

--- apps/ecommerce/views.py
+++ apps/ecommerce/views.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render
 from .models import *
+from django.shortcuts import render
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 
 
-
-def checkout(request):
-    return render(request, 'ecommerce/checkout.html')
 def contact(request):
     return render(request, 'ecommerce/contact.html')
 def detail(request):
@@ -21,6 +18,16 @@ def faqs(request):
 def about_us(request):
     return render(request, 'ecommerce/about_us.html')
 
+
+def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {"get_total_price":0, "get_total_items":0}
+    return render(request, 'ecommerce/checkout.html',{"order":order, "items":items})
 
 # @login_required(login_url="users:login", redirect_field_name="next")
 def cart(request):
