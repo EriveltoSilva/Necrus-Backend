63940fd1d11be281f694995dc711ff9aa4ebe7de
eriveltosilva <eriveltoclenio@gmail.com>
2024-04-04 12:49:45 +0100

    feat: add models db

--- apps/ecommerce/admin.py
+++ apps/ecommerce/admin.py
@@ -1,29 +1,90 @@
 from django.contrib import admin
-from .models import Customer, Product, Order, OrderItem, ShippingAddress
-# Register your models here.
+from .models import *
+
 
 class CustomerAdmin(admin.ModelAdmin):
-   list_display = ['id','user','name', 'email', 'created_at']
-   list_display_links = ['id','user','name', 'email', 'created_at']
-    
+   list_display = ['id', 'user', 'created_at']
+   list_display_links = ['id', 'user', 'created_at']
+   list_per_page = 20
+
+admin.site.register(Customer, CustomerAdmin)
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+   list_display = ['id', 'user', 'created_at']
+   list_display_links = ['id', 'user', 'created_at']
+   search_fields = ['user', 'created_at']
+   list_per_page = 20
+
+admin.site.register(Employee, EmployeeAdmin)
+
+class GenderAdmin(admin.ModelAdmin):
+   list_display = ['id', 'name', 'created_at']
+   list_display_links = ['id', 'name', 'created_at']
+   search_fields = ['name', 'created_at']
+   list_per_page = 20
+
+admin.site.register(Gender, GenderAdmin)
+
+class SizeAdmin(admin.ModelAdmin):
+   list_display = ['id', 'name', 'created_at']
+   list_display_links = ['id', 'name', 'created_at']
+   search_fields = ['name', 'created_at']
+   list_per_page = 20
+
+admin.site.register(Size, SizeAdmin)
+
+class ColorAdmin(admin.ModelAdmin):
+   list_display = ['id', 'name', 'value_hexadecimal', 'created_at']
+   list_display_links = ['id', 'name', 'value_hexadecimal', 'created_at']
+   search_fields = ['name', 'value_hexadecimal', 'created_at']
+   list_per_page = 20
+
+admin.site.register(Color, ColorAdmin)
+
+class SaleAdmin(admin.ModelAdmin):
+   list_display = ['id', 'percentage_value','expiration_date', 'created_at']
+   list_display_links = ['id','percentage_value', 'expiration_date','created_at']
+   search_fields = ['title', 'percentage_value', 'expiration_date', 'created_at']
+   list_per_page = 20
+   
+admin.site.register(Sale, SaleAdmin)
+   
+
+class ProductCategoryAdmin(admin.ModelAdmin):
+   list_display = ['id', 'name', 'image', 'created_at']
+   list_display_links = ['id', 'name', 'created_at']
+   search_fields = ['name', 'description', 'created_at']
+   list_per_page = 20
+
+admin.site.register(ProductCategory, ProductCategoryAdmin)
+
 class ProductAdmin(admin.ModelAdmin):
    list_display = ['id','name', 'price', 'created_at']
    list_display_links = ['id','name', 'price', 'created_at']
+   search_fields = ['name', 'price', 'created_at']
+   list_per_page = 20
+
+admin.site.register(Product,ProductAdmin)
+
 
 class OrderAdmin(admin.ModelAdmin):
    list_display = ['id','customer', 'complete','transaction_id', 'created_at']
    list_display_links = ['id','customer','transaction_id', 'created_at']
+   list_per_page = 20
+
+admin.site.register(Order,OrderAdmin)
 
 class OrderItemAdmin(admin.ModelAdmin):
    list_display = ['id','product', 'order','quantity', 'created_at']
    list_display_links = ['id','product', 'order','quantity', 'created_at']
+   list_per_page = 20
     
+admin.site.register(OrderItem,OrderItemAdmin)
+
 class ShippingAddressAdmin(admin.ModelAdmin):
    list_display = ['id','customer', 'order','address', 'created_at']
    list_display_links = ['id','customer', 'order','address', 'created_at']
+   list_per_page = 20
 
-admin.site.register(Customer,CustomerAdmin)
-admin.site.register(Product,ProductAdmin)
-admin.site.register(Order,OrderAdmin )
-admin.site.register(OrderItem,OrderItemAdmin)
 admin.site.register(ShippingAddress, ShippingAddressAdmin)
--- apps/ecommerce/models.py
+++ apps/ecommerce/models.py
@@ -6,18 +6,97 @@ User = get_user_model()
 # Create your models here.
 class Customer(models.Model):
     user = models.OneToOneField(to=User, on_delete=models.SET_NULL, null=True, blank=True)
-    name = models.CharField(max_length=250, null=False)
-    email = models.EmailField(max_length=250, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self) -> str:
+        return self.get_full_name()
+    
+    def get_full_name(self):
+        return self.user.get_full_name()
+
+class Employee(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.get_full_name()
+    
+    def get_full_name(self):
+        return self.user.get_full_name()
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=200, null=False)
+    description = models.TextField()
+    image = models.ImageField(upload_to="product-categories", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except ValueError as e:
+            print("## ERRO CARREGANDO A IMAGEM da CATEGORIA:", self.name)
+            url=''
+        return url
+
+class Gender(models.Model):
+    name = models.CharField(max_length=30,  null=False)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Size(models.Model):
+    name = models.CharField(max_length=5, null=False)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
         return self.name
 
+class Color(models.Model):
+    name = models.CharField(max_length=20, null=False)
+    description = models.TextField()
+    value_hexadecimal = models.CharField(max_length=7, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.value_hexadecimal}"
+
+
+    
+class Sale(models.Model):
+    title = models.CharField(max_length=100)
+    percentage_value = models.FloatField()
+    expiration_date = models.DateTimeField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return f"{self.percentage_value}"
+    
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
     image = models.ImageField(upload_to="products", null=True, blank=True)
+
+    category = models.ManyToManyField(to=ProductCategory)
+    gender = models.ManyToManyField(to=Gender)
+    color = models.ManyToManyField(to=Color)
+    size = models.ManyToManyField(to=Size)
+    sale = models.ForeignKey(to=Sale, on_delete=models.CASCADE, null=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
@@ -34,7 +113,7 @@ class Product(models.Model):
         return url
     
 class Order(models.Model):
-    customer = models.ForeignKey(to=Customer, on_delete=models.SET_NULL, null=True)
+    customer = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
     complete = models.BooleanField(default=False)
     transaction_id = models.CharField(max_length=100, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -55,6 +134,16 @@ class OrderItem(models.Model):
     def __str__(self) -> str:
         return f"{self.id} - {self.order}"
 
+class Review(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    comment = models.TextField()
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.user}"
+    
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(to=Customer, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(to=Order, on_delete=models.CASCADE)
