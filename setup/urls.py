9da16d4816720ae19fdd21d215bed3eae04e6b0f
eriveltosilva <eriveltoclenio@gmail.com>
2024-04-10 02:34:42 +0100

    docs: update restframework installation

--- setup/urls.py
+++ setup/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.ecommerce.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
