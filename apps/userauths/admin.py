3a5b5942e125d0bdc6ed91968617e00046f94b6d
eriveltosilva <eriveltoclenio@gmail.com>
2024-04-17 09:14:45 +0100

    admin ussrauths

--- apps/userauths/admin.py
+++ apps/userauths/admin.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
 from .models import User, Profile
 
-# class UserAdmin(admin.ModelAdmin):
-#     list_display = ['id', 'full_name', 'username','email']
-#     list_display_links = ['id', 'full_name', 'username','email']
-#     search_fields = ['full_name', 'username','email']
-#     list_per_page = 20
-# admin.site.register(User, UserAdmin)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'full_name', 'username','email']
+    list_display_links = ['id', 'full_name', 'username','email']
+    search_fields = ['full_name', 'username','email']
+    list_per_page = 20
+admin.site.register(User, UserAdmin)
 
 
-# class ProfileAdmin(admin.ModelAdmin):
-#     list_display = ['id', 'full_name', 'gender','created_at']
-#     list_display_links = ['id', 'full_name', 'gender','created_at']
-#     search_fields = ['full_name', 'gender','created_at']
-#     list_per_page = 20
-# admin.site.register(Profile, ProfileAdmin)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'full_name', 'gender','created_at']
+    list_display_links = ['id', 'full_name', 'gender','created_at']
+    search_fields = ['full_name', 'gender','created_at']
+    list_per_page = 20
+admin.site.register(Profile, ProfileAdmin)
