f4caa41d5c672c5713b512ceab4aad212d875569
eriveltosilva <eriveltoclenio@gmail.com>
2024-04-18 23:56:57 +0100

    feat:authentication token refresh route

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -2,9 +2,10 @@ from . import views
 from rest_framework import routers
 from django.urls import path, include
 from apps.userauths import views as userauths_views
-
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('user/token/', userauths_views.MyTokenObtainPairView.as_view()),    
+    path('user/token/refresh', TokenRefreshView.as_view()),    
     path('user/register/', userauths_views.RegisterView.as_view()),    
 ]
